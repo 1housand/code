0d23195595b51cd62c4220729d54105c32807d56
Jason Yeo <jasonyeo224@gmail.com>
2016-01-12 17:14:22 -0500

    push

---
+++ CS323Proj2/src/Maze.java
@@ -0,0 +1,16 @@
+/*
+ * CS323 Project 2
+ * Jason Yeo
+ */
+
+public class Maze{ //Object for each maze block with necessary variables.
+
+	int r, c;
+	boolean visited;
+	boolean[] walls = new boolean[3];
+}
+	
+	
+	
+	
+	
